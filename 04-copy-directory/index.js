edbc902a78fee11882fde13946ea22fe1227bbc1
Egor Kaeta <kaeta.egor2015@yandex.ru>
2022-11-05 01:06:44 +0300

    feat: implement 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
+const path = require('path');
+const fs = require('fs');
+const { mkdir, rm } = require('fs/promises');
 
+
+const dirPath = path.join(__dirname, 'files');
+const newDir = path.join(__dirname, 'files-copy');
+
+async function copyDir(dirName, newDirName) {
+  await rm(newDirName,   {recursive: true, force: true});
+  await mkdir(newDirName, {recursive: true});
+  fs.readdir(dirName, 
+    { withFileTypes: true },
+    (err, files) => {
+      console.log('\nКопирование файлов');
+      if (err)
+        console.log(err);
+      else {
+        files.forEach(file => {
+          if (file.isDirectory()) {
+            let dirInDirPath = path.join(dirName, file.name);
+            let newDirInDirPath = path.join(newDirName, file.name);
+            copyDir(dirInDirPath, newDirInDirPath);
+          }
+          else {
+            fs.copyFile(path.join(dirName, file.name), path.join(newDirName, file.name), err => {
+              if(err) throw err;
+              console.log('Файл успешно скопирован');
+            });
+          }
+        });
+      }
+    });
+}
+
+copyDir(dirPath, newDir);
